dfb399790b1762541b7c5880060435e4ebaaf22c
nicole <ramosmikaelanicole@gmail.com>
2024-08-04 10:48:51 +0800

    Initial Commit

---
+++ data-structures/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  data-structures
+//
+//  Created by Mikaela Nicole Ramos on 8/4/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
